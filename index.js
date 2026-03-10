6b31a54772bebff0170039edc176974b2d868ef5
aniruddhasmg333-hub <aniruddhasmg333@gmail.com>
2026-03-11 01:15:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, { maxHttpBufferSize: 1e7 });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
+  // When a message is sent
   socket.on('chat message', (data) => {
-    io.emit('chat message', data); 
+    data.id = Date.now() + Math.random(); // Unique ID for each message
+    data.time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    data.readBy = []; // List of people who saw it
+    io.emit('chat message', data);
+  });
+
+  // When a user "reads" a message
+  socket.on('message read', (readData) => {
+    io.emit('update read status', readData);
   });
 });
 
 http.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+  console.log('Server running with Read Receipts on port ' + PORT);
 });
