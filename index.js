9fe172660984fa4e876136d706e8cab90f8ca75f
aniruddhasmg333-hub <aniruddhasmg333@gmail.com>
2026-03-11 00:38:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-  console.log('A user joined');
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Sends message to everyone
+  console.log('A user connected');
+  
+  socket.on('chat message', (data) => {
+    // This now sends the name and the message together
+    io.emit('chat message', data);
   });
 });
 
