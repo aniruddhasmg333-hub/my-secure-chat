c845a1aa69471bab9ebb637d7eb6bebad6e18c54
aniruddhasmg333-hub <aniruddhasmg333@gmail.com>
2026-03-11 02:13:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, { maxHttpBufferSize: 1e7 });
+const io = require('socket.io')(http, { maxHttpBufferSize: 1e8 }); // Increased to 100MB for audio
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
   socket.on('chat message', (data) => {
-    // Exact timestamp logic
     const now = new Date();
     data.time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     data.id = Date.now() + Math.random(); 
     
-    // Secret command to clear chat for everyone
     if (data.msg === "/clear") {
         io.emit('clear chat');
     } else {
@@ -27,5 +25,5 @@ io.on('connection', (socket) => {
 });
 
 http.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
+  console.log('Server running with Voice Support on port ' + PORT);
 });
